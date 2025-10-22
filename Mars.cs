3b03ab5e9bdabfd50a59fe904f7f2ee5800fcebd
kd40g <140960745+kd40g@users.noreply.github.com>
2025-10-22 11:12:36 -0500

    .dll upload

---
+++ Mars.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceObjectsLibrary
+{
+    public class Mars : SpaceObjects {
+        private int x, y, z;
+        private int radius;
+
+        public Mars() { }
+
+        public Mars (int xValue, int yValue, int zValue, int radiusValue) {
+            Radius = radiusValue;
+        }
+
+        public int Radius
+        {
+            get { return radius;  }
+
+            set { if (value >= 0)
+                    radius = value; }
+
+        }
+
+        public override string Name
+        {
+            get { return "Mars"; }
+        }
+
+    }
+}
---
+++ SpaceShip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceObjectsLibrary
+{
+    public class SpaceShip : SpaceObjects {
+        private int x, y, z;
+        public SpaceShip() { }
+
+        public override string Name
+        {
+            get { return "SpaceShip"; }
+        }
+    }
+}
