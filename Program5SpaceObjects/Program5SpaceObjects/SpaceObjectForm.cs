cc1f7f4748b3a0b35300d82b52cdaec07fe8e9ce
kd40g <140960745+kd40g@users.noreply.github.com>
2025-10-22 13:59:50 -0500

    Implement space object creation and type selection
    Enhanced InstantObjectBtn_Click to create various space objects based on user input. Added error handling for input validation and improved TypeBtn_Click for type selection.

--- Program5SpaceObjects/Program5SpaceObjects/SpaceObjectForm.cs
+++ Program5SpaceObjects/Program5SpaceObjects/SpaceObjectForm.cs
@@ -16,17 +16,88 @@ namespace Program5SpaceObjects
 
         private void CreateSOBtn_Click(object sender, EventArgs e)
         {
-            // This should just enable the textbox and buttons for the object type
+            //.TextBox = true; 
         }
 
         private void InstantObjectBtn_Click(object sender, EventArgs e)
         {
-            // THis should create the object using the data entered and using the object constructor
+            // Get what the user typed
+            string type = objectTypeTextBox.Text.Trim().ToLower();
+
+            SpaceObject spaceObj = null; // will hold whatever object we create
+
+            try
+            {
+                // Read basic inputs (all objects have at least these)
+                string name = nameTextBox.Text;
+                double x = Convert.ToDouble(xTextBox.Text);
+                double y = Convert.ToDouble(yTextBox.Text);
+                double z = Convert.ToDouble(zTextBox.Text);
+
+                switch (type)
+                {
+                    case "Star":
+                        int radius = Convert.ToInt32(radiusTextBox.Text);
+                        int temp = Convert.ToInt32(tempTextBox.Text);
+                        spaceObj = new Star(name, radius, temp, x, y, z);
+                        break;
+
+                    case "Planet":
+                        int moons = Convert.ToInt32(moonsTextBox.Text);
+                        int planetRadius = Convert.ToInt32(radiusTextBox.Text);
+                        spaceObj = new Planet(name, planetRadius, moons, x, y, z);
+                        break;
+
+                    case "Spaceship":
+                        int payload = Convert.ToInt32(payloadTextBox.Text);
+                        int speed = Convert.ToInt32(speedTextBox.Text);
+                        spaceObj = new SpaceShip(name, payload, speed, x, y, z);
+                        break;
+        
+                    case "Probe":
+                        int samples = Convert.ToInt32(samplesTextBox.Text);
+                        spaceObj = new Probe(name, samples, x, y, z);
+                        break;
+        
+                    case "Earthling":
+                        int heightE = Convert.ToInt32(heightTextBox.Text);
+                        spaceObj = new Earthling(name, heightE, x, y, z);
+                        break;
+        
+                    case "Alien":
+                        int arms = Convert.ToInt32(armsTextBox.Text);
+                        int heightA = Convert.ToInt32(heightTextBox.Text);
+                        spaceObj = new Alien(name, arms, heightA, x, y, z);
+                        break;                        
+                }
+
+                // Show the object's info
+                displayLabel.Text = spaceObj.GetInfo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}\nPlease check your inputs.");
+            }
         }
+
 
         private void TypeBtn_Click(object sender, EventArgs e)
         {
-            // This should enable components based on the type of object selected
+            if (TypeBox.Text == "Star")
+
+            else if (TypeBox.Text == "Planet")
+
+            else if (TypeBox.Text == "SpaceShip")
+
+            else if (TypeBox.Text == "Earthling")
+
+            else if (TypeBox.Text == "Alien")
+
+            else if (TypeBox.Text == "Probe")
+
+            else {
+                MessageBox.Show("Invalid type! Please enter a valid SpaceObject type (Star, Planet, Probe, etc.)\nRemember to capitalize!");
+            }
         }
 
         private void MoveBtn_Click(object sender, EventArgs e)
@@ -36,12 +107,12 @@ namespace Program5SpaceObjects
 
         private void RotateBtn_Click(object sender, EventArgs e)
         {
-            // call the rotate method
+            this.Rotation(); 
         }
 
         private void ClassificationBtn_Click(object sender, EventArgs e)
         {
-            // call the classification method
+            this.DetermineClassification();
         }
 
         private void SampleBtn_Click(object sender, EventArgs e)
@@ -55,3 +126,4 @@ namespace Program5SpaceObjects
         }
     }
 }
+
